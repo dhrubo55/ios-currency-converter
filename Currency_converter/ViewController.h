5c558ad730b57cbc3298f573c6bd9aab847d4e93
mac-lab <mac-lab@mac-labs-mac-mini.local>
2018-09-08 11:09:17 +0600

    UI design for currency converter completed and binded

--- Currency_converter/ViewController.h
+++ Currency_converter/ViewController.h
@@ -9,6 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *display;
+@property (strong, nonatomic) IBOutlet UILabel *us_currency_show;
+
+
+@property (strong, nonatomic) IBOutlet UILabel *china_currency_show;
+
+@property (strong, nonatomic) IBOutlet UILabel *russia_currency_show;
+
+
+@property (strong, nonatomic) IBOutlet UILabel *india_currency_show;
+
 
 
 @end
